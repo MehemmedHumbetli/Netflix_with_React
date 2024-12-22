432c1d670763f5a55dea49afd9cc94dcef821b78
Humbetli Mehemmed Cabir <Humbe_li59@itstep.edu.az>
2024-12-22 14:23:01 +0400

    some touches

--- src/pages/landing/components/ReasonCard.jsx
+++ src/pages/landing/components/ReasonCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
  
 const ReasonCard = ({item}) => {
   return (
-    <div className='bg-gradient-to-r from-[#192247] to-[#210E17] w-[400px] h-[268px] rounded-[16px] px-4 pt-6 relative mb-[64px]'>
+    <div className='bg-gradient-to-br from-[#192247] to-[#210E17] w-[400px] h-[268px] rounded-[16px] px-4 pt-6 relative mb-[64px]'>
       <h3 className='text-white text-[24px] font-media'>{item.title}</h3>
       <p className='text-white/70 mt-2'>{item.desc}</p>
       <img className='size-[80px] absolute right-4 bottom-4' src={item.img} alt="" />
